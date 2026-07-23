9a8a6b4c7fe5f7b2c18ff386e5147ffbc3756747
Vaidik-Gampawar <vaidikgampawar@gmail.com>
2026-07-23 18:59:37 +0530

    READ Operation with Database Completed

---
+++ 16. READ Operation with Database/main.py
@@ -0,0 +1,67 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from fastapi import FastAPI, Depends, HTTPException
+
+app = FastAPI()
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+sessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    completed = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Create Todo
+@app.post("/todos")
+def create_todo(title: str, db: Session = Depends(get_db)):
+    todo = Todo(title=title, completed="False")
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    
+    return {
+        "message": "Todo Created",
+        "data": todo
+    }
+
+
+# Read All Data
+@app.get("/todos")
+def get_todos(db: Session = Depends(get_db)):
+    todo = db.query(Todo).all()
+    
+    return {
+        "Total": len(todo),
+        "data": todo
+    }
+
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    
+    if not todo:
+        raise HTTPException(
+            status_code=404,
+            detail="Todo Not Found"
+        )
+    return {
+        "message": "Todo Found",
+        "data": todo
+    }
